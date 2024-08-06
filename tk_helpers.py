69ae96c95932886d74a4e83d8e548e8cfa26ab27
David Vaz Teixeira <dvt4qb@gmail.com>
2024-08-06 01:12:51 -0300

    add helper

---
+++ tk_helpers.py
@@ -0,0 +1,43 @@
+# Simple TKinter - tk_helpers.py
+# layout helper
+
+# Nested 2D for over all components in layout
+def create(layout):
+    for r in range(len(layout)):
+        for c in range(len(layout[r])):
+            obj = layout[r][c]
+            if obj:
+                obj.grid(row=r, column=c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 1. Helper movido para arquivo tk_helpers.py
+# 2. Método renomeado para create
+# 3.
+# 4.
+# 5.
+# 6.
+# 7.
+# 8.
+# 9. 
